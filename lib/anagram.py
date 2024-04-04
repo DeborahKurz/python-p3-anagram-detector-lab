b6d397cad0e3dc44313d304c1c8a5e6f8f43e8f4
Deborah Kurz <DeborahKurz6@gmail.com>
2024-04-04 09:47:50 -0600

    Complete Lab - Walked thorugh with Professor

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+
+    def match(self, word_list):
+        sorted_word = sorted([letter for letter in self.word])
+        # matches = []
+        # for w in word_list:
+        #     sorted_w = sorted([letter for letter in w])
+        #     if sorted_word == sorted_w:
+        #         matches.append(w)
+        # return matches
+        return [w for w in word_list if sorted([letter for letter in w]) == sorted_word]
